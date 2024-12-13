8d19d18016ed1205255fbb64547445179954d1ad
Marco <marco.desing@gmail.com>
2024-12-13 17:19:07 +0100

    Proyecto

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"], // Ajusta esta ruta según la estructura de tu proyecto
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
+
---
+++ vite.config.js
@@ -0,0 +1,5 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    root: 'src', // Define la carpeta raíz
+});
