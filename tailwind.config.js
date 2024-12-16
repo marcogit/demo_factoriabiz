e8d593a27a2bbb1c3a888f309b17fc4ac783ae7f
Marco <marco.desing@gmail.com>
2024-12-16 16:32:09 +0100

    Ajustes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"], // Ajusta esta ruta según la estructura de tu proyecto
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1300px',
+    },
+    extend: {
+      fontFamily: {
+        sans: ['"DM Sans"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
